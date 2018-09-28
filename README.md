d141b876cc1df1ee944217bc1e815667dfe5d7f5
zhangyiyong <287069934@qq.com>
2018-09-28 15:30:27 +0800

    doc：修改readme文档

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # houseSpider-backend-node
 An back-end node project spider about house.
 
-### Demo
+### run
 
-1. dangdang http://localhost:3000/book/book_rank
+1. git clone https://github.com/zyyrabbit/houseSpider-backend-node.git
+2. npm install
+3. npm run dev
+4. 打开postMan 输入地址 http://127.0.0.1:3000/house/douban?pageNo=30&positionCity=广州&keywords=天河客运站
+ - pageNo： 爬取页数
+ - positionCity：定位城市
+ - keywords: 关键字（支持多个关键字）
+
+目前支持的城市列表，可以自行添加其他豆瓣的城市租房：
+
+![address](https://user-gold-cdn.xitu.io/2018/9/28/1661f136280ee47d?w=1179&h=713&f=png&s=155678)
+
+结果如下图所示：
+
+![result](https://user-gold-cdn.xitu.io/2018/9/28/1661f13c7756d750?w=1171&h=791&f=png&s=81627)
+
+5. 支持mysql数据库存储，配置文件
+
+![db](https://user-gold-cdn.xitu.io/2018/9/28/1661f128071a6af4?w=994&h=654&f=png&s=92661)
+
--- controllers/house/douban.controller.js
+++ controllers/house/douban.controller.js
@@ -3,107 +3,107 @@ import BaseDAO from '../../model/dbConnection.js'
 import { dataFormat } from '../../utils/utils.js'
 import { doubanUrls } from '../../utils/addressMap.js'
 import mapLimit from 'async/mapLimit'
-	// 根据设置的页数生成相应链接
-	function generateUrls(pageNo = 1, pageSize = 25) {
-		let pageNos = []
-		pageNo = parseFloat(pageNo)
-		if (isNaN(pageNo) || pageNo < 0 || pageNo > 100) {
-			console.error('页数格式不正确或者范围不正确！')
-			return pageNos
-		}
-		let i = -1
-		while (++i < pageNo) {
-			pageNos.push(i * 25)
-		}
+// 根据设置的页数生成相应链接
+function generateUrls(pageNo = 1, pageSize = 25) {
+	let pageNos = []
+	pageNo = parseFloat(pageNo)
+	if (isNaN(pageNo) || pageNo < 0 || pageNo > 100) {
+		console.error('页数格式不正确或者范围不正确！')
 		return pageNos
 	}
-
-	function filterDataByKeywords(title, keywords) {
-		// 只有当关键字存在才检测
-		return !keywords || keywords.some(keyword => {
-			return new RegExp(`${keyword}`).test(title)
-		})
+	let i = -1
+	while (++i < pageNo) {
+		pageNos.push(i * 25)
 	}
+	return pageNos
+}
 
-	// 爬指定单页面数据
-	async function fetchData(params, callback) {
-		console.time('  耗时')
-		let	$ = await htmlStringGet({
-			url: params.url
-		}, 'utf-8')
-		let contents = []
-		$('#content .olt tr').each(function() {
-			let info = $(this)
-			// 标题
-			let a = info.find('.title a')
-			// 获取标题
-			let title = a.attr('title') + ''
-			// 获取链接
-			let url = a.attr('href')
-			// 获取价格
-			let price = title.match(/\d{3,}/)
-			price = price ? +price[0] : 100000
-			// 获取发布时间以及数据格式化
-			let time = dataFormat(info.find('.time').text())
-			let data = [
-				title,
-				url,
-				price, // 默认取值
-				time
-			]
-			if (url && filterDataByKeywords(title, params.keywords)) {
-				contents.push(data)
-			}
-		})
-		// 触发数据回调
-		callback(null, contents)
-	}
+function filterDataByKeywords(title, keywords) {
+	// 只有当关键字存在才检测
+	return !keywords || keywords.some(keyword => {
+		return new RegExp(`${keyword}`).test(title)
+	})
+}
 
-	export default async function getHouseList(req, res, next) {
-		// 收集的数据页数
-		let { pageNo, positionCity = 'default', keywords } = req.query
-		// 处理查询关键字
-		if (keywords) {
-			keywords = Array.isArray(keywords) ? [...new Set(keywords)] : [keywords]
+// 爬指定单页面数据
+async function fetchData(params, callback) {
+	console.time('  耗时')
+	let	$ = await htmlStringGet({
+		url: params.url
+	}, 'utf-8')
+	let contents = []
+	$('#content .olt tr').each(function() {
+		let info = $(this)
+		// 标题
+		let a = info.find('.title a')
+		// 获取标题
+		let title = a.attr('title') + ''
+		// 获取链接
+		let url = a.attr('href')
+		// 获取价格
+		let price = title.match(/\d{3,}/)
+		price = price ? +price[0] : 100000
+		// 获取发布时间以及数据格式化
+		let time = dataFormat(info.find('.time').text())
+		let data = [
+			title,
+			url,
+			price, // 默认取值
+			time
+		]
+		if (url && filterDataByKeywords(title, params.keywords)) {
+			contents.push(data)
 		}
-		let baseUrl = doubanUrls[positionCity] + 'discussion?start='
-		// 请求页数
-		let pageNos = generateUrls(pageNo)
-		// 控制并发
-		mapLimit(pageNos, 10, async (pageNo, callback) => {
-			await fetchData({
-				url: baseUrl + pageNo,
-				keywords
-			}, callback)
-			console.timeEnd('  耗时')
-		}, async (err, result) => {
-			if (err) {
-				console.error(err)
-			}
-			let data = result.reduce((pre, cur) => pre.concat(cur), [])
-			// 如果结果集为空，则不删除
-			if (data.length === 0) {
-				res.send({
-					code: 200,
-					data: []
-				})
-				return
-			}
-			// 每次插入之前先删除
-			await BaseDAO.delete({
-				tableName: 'douban'
-			})
-			// 批量插入数据
-			await BaseDAO.batchInsert({
-				preData: {
-					tableName: 'douban',
-					fields: ['title', 'url', 'price', 'time']
-				},
-				data: [data] // 很奇怪和文档不符合，不知道为什么加外面还要加一层[]
-			})
+	})
+	// 触发数据回调
+	callback(null, contents)
+}
+
+export default async function getHouseList(req, res, next) {
+	// 收集的数据页数
+	let { pageNo, positionCity = 'default', keywords } = req.query
+	// 处理查询关键字
+	if (keywords) {
+		keywords = Array.isArray(keywords) ? [...new Set(keywords)] : [keywords]
+	}
+	let baseUrl = doubanUrls[positionCity] + 'discussion?start='
+	// 请求页数
+	let pageNos = generateUrls(pageNo)
+	// 控制并发
+	mapLimit(pageNos, 10, async (pageNo, callback) => {
+		await fetchData({
+			url: baseUrl + pageNo,
+			keywords
+		}, callback)
+		console.timeEnd('  耗时')
+	}, async (err, result) => {
+		if (err) {
+			console.error(err)
+		}
+		let data = result.reduce((pre, cur) => pre.concat(cur), [])
+		// 如果结果集为空，则不删除
+		if (data.length === 0) {
 			res.send({
 				code: 200,
-				data: data
+				data: []
 			})
+			return
+		}
+		// 每次插入之前先删除
+		/* await BaseDAO.delete({
+			tableName: 'douban'
+		}) */
+		// 批量插入数据
+		await BaseDAO.batchInsert({
+			preData: {
+				tableName: 'douban',
+				fields: ['title', 'url', 'price', 'time']
+			},
+			data: [data] // 很奇怪和文档不符合，不知道为什么加外面还要加一层[]
 		})
-	}
+		res.send({
+			code: 200,
+			data: data
+		})
+	})
+}
